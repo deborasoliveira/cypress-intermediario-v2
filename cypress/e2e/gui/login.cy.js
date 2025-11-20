8ed743d3f46a7c068e819eb5cd9e195d3880cded
Debora Oliveira <debora.raquel710@gmail.com>
2025-11-20 18:37:36 -0300

    refactor: adjust the login process to use cy.session

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,6 +1,11 @@
 describe('Login', () => {
   it('successfully', () => {
-    cy.login()
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = {cacheSession: false}
+
+    cy.login(user, password, options)
+    
     cy.get('[data-qa-selector="user_menu"]').should('be.visible')
   })
 })
